6b17eb93f5df0d1704e2ee79c2392385d76e03e6
yyewolf <yyewolf@gmail.com>
2024-10-12 09:48:23 +0000

    fix: script

--- files/scripts/worker-cloud-init.sh
+++ files/scripts/worker-cloud-init.sh
@@ -20,4 +20,31 @@ swapoff -a
 modprobe br_netfilter
 sysctl -w net.ipv4.ip_forward=1
 
+apt-get install -y conmon
+
+wget https://downloads.nestybox.com/sysbox/releases/v0.6.4/sysbox-ce_0.6.4-0.linux_amd64.deb
+sudo apt-get install jq
+sudo apt-get install ./sysbox-ce_0.6.4-0.linux_amd64.deb
+
+sudo adduser containers
+
+cat <<EOF > /etc/subuid
+debian:100000:65536
+sysbox:165536:65536
+containers:231072:16777216
+EOF
+
+cat <<EOF > /etc/subgid
+debian:100000:65536
+sysbox:165536:65536
+containers:231072:16777216
+EOF
+
+cat <<EOF > /etc/crio/crio.conf
+[crio.runtime.runtimes.sysbox-runc]
+allowed_annotations = ["io.kubernetes.cri-o.userns-mode"]
+runtime_path = "/usr/bin/sysbox-runc"
+runtime_type = "oci"
+EOF
+
 touch /tmp/signal
